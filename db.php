10fb87d5053fca08a46f0b2cdaf35f0e08f995b3
chaitanyapanja <109240773+chaitanyapanja@users.noreply.github.com>
2022-11-21 11:06:17 -0600

    update for heroku db details

--- db.php
+++ db.php
@@ -1,16 +1,16 @@
 <?php
     // Enter your host name, database username, password, and database name.
-    // If you have not set database password on localhost then set empty.
-    // $db_host = "us-cdbr-east-06.cleardb.net";
-    //$db_user = "b0d3b9ca392ecc";
-   // $db_pass = "4f4168c7";
- // $db_name = "heroku_e53d23270615e73";
-  //  $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
+    If you have not set database password on localhost then set empty.
+    $db_host = "us-cdbr-east-06.cleardb.net";
+    $db_user = "b25f67b2227c76";
+    $db_pass = "1c6ab525";
+  $db_name = "heroku_aa566d2ccabb3a6";
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
 // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
     $con = mysqli_connect("127.0.0.1","root","","findyourtechnician");
     // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    //if (mysqli_connect_errno()){
+      //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 ?>
