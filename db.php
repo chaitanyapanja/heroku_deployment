dcde39b01e474aaf272eb34d95dcbe2cb092a472
chaitanyapanja <109240773+chaitanyapanja@users.noreply.github.com>
2022-11-21 12:33:48 -0600

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
     $db_host = "us-cdbr-east-06.cleardb.net";
     $db_user = "bd505dcb380a15";
     $db_pass = "bf4eba31";
-  $db_name = "heroku_aa566d2ccabb3a6";
+  $db_name = "heroku_184a67814c65bcc";
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
 // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
