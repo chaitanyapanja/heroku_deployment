06bbc56ba795421f0e9cc004d8f513c021382a44
chaitanyapanja <109240773+chaitanyapanja@users.noreply.github.com>
2022-11-21 11:12:46 -0600

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,7 @@
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
 // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("127.0.0.1","root","","findyourtechnician");
+    //$con = mysqli_connect("127.0.0.1","root","","findyourtechnician");
     // Check connection
     //if (mysqli_connect_errno()){
       //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
